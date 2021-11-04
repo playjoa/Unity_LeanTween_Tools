7d63b21eba7ca28b2b4b0318ab5ce2feb44d6707
Joao Milone <joaocmil@hotmail.com>
2021-11-04 20:57:49 -0300

    Added Slide Animation

---
+++ Leantween System/Data/TweenSlideAnimationData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TweenerSystem.Data
+{
+    [Serializable]
+    public class TweenSlideAnimationData
+    {
+        [SerializeField] private SlideDirection slideDirection = SlideDirection.Down;
+        [SerializeField] private LeanTweenType easeType = LeanTweenType.easeOutBack;
+        [SerializeField] private float slideDelay = 0f;
+        [SerializeField] private float slideDuration = 0.5f;
+        [SerializeField] private Vector3 slideRectTransformTarget = Vector3.zero;
+        [SerializeField] private UnityEvent onSlideComplete;
+
+        public TweenSlideAnimationData(SlideDirection slideDirection)
+        {
+            this.slideDirection = slideDirection;
+        }
+        
+        public SlideDirection Direction => slideDirection;
+        public LeanTweenType EaseType => easeType;
+        public float Delay => slideDelay;
+        public float Duration => slideDuration;
+        public Vector3 Target => slideRectTransformTarget;
+        public UnityEvent SlideComplete => onSlideComplete;
+    }
+    
+    public enum SlideDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ Leantween System/LeanTweenerUISlideAnimation.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using TweenerSystem.Data;
+using UnityEngine;
+
+namespace TweenerSystem
+{
+    
+    public class LeanTweenerUISlideAnimation : MonoBehaviour
+    {
+        [Header("Slide In Config")]
+        [SerializeField] private TweenSlideAnimationData slideAnimationData = new TweenSlideAnimationData(SlideDirection.Up);
+        [SerializeField] private TweenSlideAnimationData slideOutData = new TweenSlideAnimationData(SlideDirection.Down);
+
+        [HideInInspector] 
+        [SerializeField] private RectTransform objectRect;
+        
+        private Dictionary<SlideDirection, Vector3> targetDirections = new Dictionary<SlideDirection, Vector3>();
+
+        private void OnValidate()
+        {
+            objectRect = GetComponent<RectTransform>();
+        }
+
+        private void Awake()
+        {
+            GetDirectionsDictionary();
+        }
+
+        private void GetDirectionsDictionary()
+        {
+            var topScreen = Screen.currentResolution.height;
+            var screenWidth = Screen.currentResolution.width;
+            var rect = objectRect.rect;
+            var objectWidth = rect.width;
+            var objectHeight = rect.height;
+
+            targetDirections.Add(SlideDirection.Down, new Vector3(0, - topScreen + objectHeight / 2f, 0));
+            targetDirections.Add(SlideDirection.Up, new Vector3(0, topScreen + objectHeight / 2f, 0));
+            targetDirections.Add(SlideDirection.Left, new Vector3(- screenWidth + objectWidth / 2f, 0, 0));
+            targetDirections.Add(SlideDirection.Right, new Vector3(screenWidth + objectWidth / 2f, 0, 0));
+        }
+
+        private void OnEnable()
+        {
+            SlideIn();
+        }
+
+        private void SlideIn()
+        {
+            objectRect.localPosition = targetDirections[slideAnimationData.Direction];
+            
+            LeanTween.moveLocal(gameObject, slideAnimationData.Target, slideAnimationData.Duration)
+                .setEase(slideAnimationData.EaseType)
+                .setOnComplete(() => slideAnimationData.SlideComplete?.Invoke());
+        }
+
+        public void SlideOut()
+        {
+            LeanTween.moveLocal(gameObject, targetDirections[slideOutData.Direction], slideOutData.Duration)
+                .setEase(slideOutData.EaseType)
+                .setOnComplete(() => slideOutData.SlideComplete?.Invoke());
+        }
+    }
+}
